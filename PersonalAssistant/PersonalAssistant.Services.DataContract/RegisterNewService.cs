423246b9644f6f01dde58b82142c257173d0d8bd
Andrei Scutariu <Andrei.Scutariu@endava.com>
2016-11-19 18:27:54 +0200

    add data contract project

---
+++ PersonalAssistant/PersonalAssistant.Services.DataContract/RegisterNewService.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalAssistant.Services.DataContract
+{
+    public class RegisterNewService
+    {
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location1 { get; set; }
+
+        public string Location2 { get; set; }
+
+        public string Price { get; set; }
+    }
+}
--- PersonalAssistant/PersonalAssistant.Services.External/Class1.cs
+++ PersonalAssistant/PersonalAssistant.Services.External/Class1.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PersonalAssistant.Services.External
+﻿namespace PersonalAssistant.Services.External
 {
     public class Class1
     {
