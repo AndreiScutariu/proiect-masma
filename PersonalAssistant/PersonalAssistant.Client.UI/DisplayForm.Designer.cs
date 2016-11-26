78c4e6b6a699a0c471244aae7ca034e08163b36e
Andrei Scutariu <Andrei.Scutariu@endava.com>
2016-11-26 16:58:39 +0200

    add ui for personal assistant

---
+++ PersonalAssistant/PersonalAssistant.Client.UI/DisplayForm.Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonalAssistant.Client.UI
+{
+    partial class DisplayForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 600);
+            Name = "DisplayForm";
+            Text = "Personal Assistant";
+
+            ResumeLayout(false);
+        }
+    }
+}
+
---
+++ PersonalAssistant/PersonalAssistant.Client.UI/DisplayForm.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalAssistant.Client.UI
+{
+    using System.Windows.Forms;
+
+    public partial class DisplayForm : Form
+    {
+        public DisplayForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
